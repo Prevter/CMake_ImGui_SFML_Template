9f2b2dda7cb1c648194a5482b58336b9c0d40c9e
Nemesh <prevter@gmail.com>
2022-08-10 21:59:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## **SFML + ImGui** CMake Template
+This is a template for creating a CMake project using SFML and ImGui.
---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+﻿#include "imgui.h"
+#include "imgui-SFML.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <string>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(1280, 720)), "SFML + ImGui Template");
+    ImGui::SFML::Init(window);
+
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_::ImGuiConfigFlags_DockingEnable;
+
+    sf::Clock deltaClock;
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        sf::Time deltaTime = deltaClock.restart();
+
+        ImGui::SFML::Update(window, deltaTime);
+		
+        ImGui::ShowDemoWindow();
+
+        window.clear();
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+    return 0;
+}
